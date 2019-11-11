17b0998a07cd8257c2a32a66d53d931fb75fb193
NguyenPhuongVuong <phuongvuong@Tris-MacBook-Pro.local>
2019-11-11 17:06:02 +0700

    edit base.html

--- app/CRUD/city/views.py
+++ app/CRUD/city/views.py
@@ -3,6 +3,6 @@ from flask import Blueprint, render_template
 city_blueprint = Blueprint('city', __name__, template_folder='templates')
 
 
-@city_blueprint.route('/', methods=['GET'])
+@city_blueprint.route('/create', methods=['GET'])
 def city():
-    return render_template('base.html')
+    return render_template('CRUD/city/create.html')
