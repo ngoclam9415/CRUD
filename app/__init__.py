e5d79c2918762b375a1ad5d145ce315ae9d6b77d
Nguyen Ngoc Lam <lamnn@athena.studio>
2019-11-12 03:46:23 +0000

    Create district

--- app/__init__.py
+++ app/__init__.py
@@ -2,22 +2,17 @@
 import os
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
-
-
 from config import config
 db = SQLAlchemy()
-
 
 def create_app(config_name):
     app = Flask(__name__)
     app.config.from_object(config[config_name])
     config[config_name].init_app(app)
-
     db.init_app(app)
-
-    from .models import City
-
     from app.CRUD.city.views import city_blueprint
+    from app.CRUD.district.views import district_blueprint
     app.register_blueprint(city_blueprint, url_prefix='/city')
+    app.register_blueprint(district_blueprint, url_prefix='/district')
 
     return app
--- app/models.py
+++ app/models.py
@@ -1,13 +1,25 @@
-from app import db
+from app import db, create_app
 
 class City(db.Model):
     __tablename__ = 'city'
+    __table_args__ = {'extend_existing': True} 
 
     id = db.Column(db.Integer, primary_key=True, autoincrement=True)
-    name = db.Column(db.String(255))
+    name = db.Column(db.String(255), unique=True)
+    district = db.relationship("District", backref='city', lazy="dynamic")
 
-    def __init__(self, name):
-        self.name = name
+    # def __init__(self, name):
+    #     self.name = name
 
     def __repr__(self):
         return '<City %r>' % self.name
+
+class District(db.Model):
+    __tablename__ = "district"
+    __table_args__ = {'extend_existing': True} 
+
+    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    name = db.Column(db.String(50))
+    city_id = db.Column(db.Integer, db.ForeignKey('city.id'))
+
+
