b1ee7c9ec9869b67272e54924f25dcf424889c63
Nguyen Ngoc Lam <lamnn@athena.studio>
2019-11-12 11:17:59 +0000

    Add features List and Edit District

--- app/static/district/district.js
+++ app/static/district/district.js
@@ -1,34 +1,53 @@
-var create_district = window.location.origin + "/district/create"
+var create_district = window.location.origin + "/district/api/create";
+var edit_district = window.location.origin + "/district/api/edit";
 
-$("#exampleModal").on("shown.bs.modal", function(){
-    console.log("POP UP")
-})
+$("#exampleModal").on("shown.bs.modal", function() {
+  console.log("POP UP");
+});
 
-$("form").on("submit", function(event){
-    event.preventDefault();
-    var district_name = $("#district").val();
-    var city_name = $("#city").val();
-    var data = {"district_name" : district_name, "city_name" : city_name};
-    send_district_info(create_district, data).then(response => {
-        console.log(response);
-    })
-    
-})
+$("#modify").on("click", function(event) {
+  event.preventDefault();
+  var district_name = $("#district_edit").val();
+  var city_name = $("#city_edit").val();
+  var district_id = $("#id_edit").val();
+  var data = {
+    district_name: district_name,
+    city_name: city_name,
+    district_id: district_id
+  };
+  send_district_info(edit_district, data).then(response => {
+    window.location.href = window.location.href;
+  });
+});
 
-$(".btn.btn-info").on("click", function(){
-    var this_row = $(this).closest("tr");
-    var district_id = this_row.find("td").eq(0).val();
-    var district_name = this_row.find("td").eq(1).val();
-    var city_name = this.row.find("td").eq(2).val();
-})
+$(".btn.btn-info").on("click", function() {
+  var this_row = $(this).closest("tr");
+  console.log(this_row);
+  var district_id = this_row
+    .find("td")
+    .eq(0)
+    .text();
+  var city_name = this_row
+    .find("td")
+    .eq(1)
+    .text();
+  var district_name = this_row
+    .find("td")
+    .eq(2)
+    .text();
+  console.log(city_name);
+  $("#district_edit").val(district_name);
+  $("#city_edit").val(city_name);
+  $("#id_edit").val(district_id);
+});
 
-async function send_district_info(url, data){
-    const response = await fetch(url, {
-        method: "POST",
-        headers: {
-            'Content-Type' : 'application/json'
-        },
-        body : JSON.stringify(data),
-    });
-    return await response.json();
+async function send_district_info(url, data) {
+  const response = await fetch(url, {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json"
+    },
+    body: JSON.stringify(data)
+  });
+  return await response.json();
 }
