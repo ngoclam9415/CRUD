42351cea3068648125b1aa55d26aa7cb7b2d56c2
Nguyen Ngoc Lam <lamnn@athena.studio>
2019-11-13 05:09:44 +0000

    Create List Edit Color

--- app/__init__.py
+++ app/__init__.py
@@ -6,6 +6,7 @@ from flask_sqlalchemy import SQLAlchemy
 from config import config
 db = SQLAlchemy()
 
+
 def create_app(config_name):
     app = Flask(__name__)
     CORS(app)
@@ -14,8 +15,10 @@ def create_app(config_name):
     db.init_app(app)
     from app.CRUD.city.views import city_blueprint
     from app.CRUD.district.views import district_blueprint
+    from app.CRUD.color.views import color_blueprint
     app.register_blueprint(city_blueprint, url_prefix='/city')
     app.register_blueprint(district_blueprint, url_prefix='/district')
+    app.register_blueprint(color_blueprint, url_prefix='/color')
 
     from app.CRUD.address.views import address_blueprint
     app.register_blueprint(address_blueprint, url_prefix='/address')
