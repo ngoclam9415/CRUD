37c6a5a80141c77ee1f9ea47da6e82078a35d375
Nguyen Ngoc Lam <lamnn@athena.studio>
2019-11-18 17:49:29 +0700

    Finish refractor Brand

--- app/CRUD/brand/views.py
+++ app/CRUD/brand/views.py
@@ -2,6 +2,7 @@ from flask import Blueprint, render_template, request, make_response, jsonify, r
 
 from database.mysql_access.models import db
 from database.mysql_access.models import Brand
+from database import access_factory
 
 brand_blueprint = Blueprint('brand', __name__, template_folder='templates')
 
@@ -9,39 +10,23 @@ brand_blueprint = Blueprint('brand', __name__, template_folder='templates')
 @brand_blueprint.route('/api/list', methods=['GET'])
 def list_brand_api():
     page = request.args.get('page', 1, type=int)
-    cities = Brand.query.paginate(page, 10, error_out=False)
-    total_pages = cities.pages
-    arr_brand = []
-    for brand in cities.items:
-        tmp_brand = {
-            'id': brand.id,
-            'name': brand.name
-        }
-        arr_brand.append(tmp_brand)
-    res = {
-        "total_pages": total_pages,
-        "data": arr_brand,
-    }
+    res = access_factory.get_access("brand").list_item(page=page)
     return make_response(jsonify(res), 200)
 
 
 @brand_blueprint.route('/', methods=['GET'])
 def list_brand():
     page = request.args.get('page', 1, type=int)
-    cities = Brand.query.paginate(page, 10, error_out=False)
-    total_pages = cities.pages
-    return render_template('CRUD/brand/list.html', total_pages=total_pages, brand_active="active")
+    res = access_factory.get_access("brand").list_item(page=page)    
+    return render_template('CRUD/brand/list.html', total_pages=res["total_pages"], brand_active="active")
 
 
 @brand_blueprint.route('/create', methods=['GET', 'POST'])
 def create_brand(error=None):
     if request.method == 'POST':
         brand_name = request.form['brandName']
-        brand_exist = Brand.query.filter_by(name=brand_name).first()
-        if brand_exist is None and brand_name != "":
-            new_brand = Brand(name=brand_name)
-            db.session.add(new_brand)
-            db.session.commit()
+        if access_factory.get_access("brand").verify_qualified_item(name=brand_name) and brand_name != "":
+            access_factory.get_access("brand").create_item(name=brand_name)
             return redirect('/brand')
         else:
             error = "Your brand is error"
@@ -52,7 +37,6 @@ def create_brand(error=None):
 def edit_brand():
     brand_id = request.form['brand_id']
     brand_name = request.form['brand_name']
-    db.session.query(Brand).filter(
-        Brand.id == brand_id).update({"name": brand_name})
-    db.session.commit()
+    if brand_name != "" and access_factory.get_access("brand").verify_qualified_item(name=brand_name):
+        access_factory.get_access("brand").edit_item(brand_id, name=brand_name)
     return redirect('/brand')
