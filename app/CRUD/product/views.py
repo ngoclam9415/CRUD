6921ae78f5f947ccc95084e6dfd61cd7ee8259c0
Nguyen Ngoc Lam <lamnn@athena.studio>
2019-11-19 15:10:20 +0700

    Finish Refractor Product

--- app/CRUD/product/views.py
+++ app/CRUD/product/views.py
@@ -1,38 +1,24 @@
-from flask import Blueprint, render_template, request, make_response, jsonify, redirect
+from flask import Blueprint, render_template, request, make_response, jsonify, redirect, flash
 
 from database.mysql_access.models import db
-from database.mysql_access.models import City, Category, Brand, Product
-
+from database.mysql_access.models import Category, Product
+from database import access_factory
 product_blueprint = Blueprint('product', __name__, template_folder='templates')
 
 
 @product_blueprint.route('/api/list', methods=['GET'])
 def list_product_api():
     page = request.args.get('page', 1, type=int)
-    products = Product.query.paginate(page, 10, error_out=False)
-    total_pages = products.pages
-    arr_product = []
-    for product in products.items:
-        tmp_product = {
-            'id': product.id,
-            'name': product.name,
-            'category_name': Category.query.get_or_404(int(product.category_id)).name
-        }
-        arr_product.append(tmp_product)
-    res = {
-        "total_pages": total_pages,
-        "data": arr_product,
-    }
+    res = access_factory.get_access("product").list_item(page=page)
     return make_response(jsonify(res), 200)
 
 
 @product_blueprint.route('/', methods=['GET'])
 def list_product():
     page = request.args.get('page', 1, type=int)
-    products = Product.query.paginate(page, 10, error_out=False)
-    total_pages = products.pages
-    categories = Category.query.all()
-    return render_template('CRUD/product/list.html', total_pages=total_pages, categories=categories, product_active="active")
+    res = access_factory.get_access("product").list_item(page=page)
+    categories = access_factory.get_access("product").get_categories()
+    return render_template('CRUD/product/list.html', total_pages=res["total_pages"], categories=categories, product_active="active")
 
 
 @product_blueprint.route('/create', methods=['GET', 'POST'])
@@ -41,22 +27,21 @@ def create_product(error=None):
     if request.method == 'POST':
         product_name = request.form['product_name']
         category_id = request.form['category_id']
-        product_exist = Category.query.filter_by(name=product_name).first()
-        if product_exist is None and product_name != "":
-            new_product = Product(name=product_name, category_id=category_id)
-            db.session.add(new_product)
-            db.session.commit()
-            return redirect('/product')
+        if product_name != "" and access_factory.get_access("product").verify_qualified_item(name=product_name, category_id=category_id):
+            access_factory.get_access("product").create_item(name=product_name, category_id=category_id)
         else:
-            error = "Your product is error"
-    return render_template('CRUD/product/create.html', categories=categories, error=error, product_active="active")
+            flash("PRODUCT INPUT INVALID", "error")
+        return redirect('/product')
+    return render_template('CRUD/product/create.html', categories=categories, product_active="active")
 
 
 @product_blueprint.route('/edit', methods=['POST'])
 def edit_product():
-    product_id = request.form['product_id']
-    product_name = request.form['product_name']
-    category_id = request.form['category_id']
-    db.session.query(Product).filter(Product.id == product_id).update({"name": product_name, "category_id": int(category_id)})
-    db.session.commit()
+    product_id = request.form.get('product_id', None)
+    product_name = request.form.get('product_name', None)
+    category_id = request.form.get('category_id', None)
+    if product_name != "" and access_factory.get_access("product").verify_qualified_item(name=product_name, category_id=category_id):
+        access_factory.get_access("product").edit_item(product_id, name=product_name, category_id=category_id)
+    else:
+        flash("PRODUCT INPUT INVALID", "error")
     return redirect('/product')
--- database/mysql_access/AccessFactory.py
+++ database/mysql_access/AccessFactory.py
@@ -5,9 +5,9 @@ from database.mysql_access.implements.CategoryDataAccess import CategoryDataAcce
 from database.mysql_access.implements.ColorDataAccess import ColorDataAccess
 from database.mysql_access.implements.AddressDataAccess import AddressDataAccess
 from database.mysql_access.implements.StoreDataAccess import StoreDataAccess
+from database.mysql_access.implements.ProductDataAccess import ProductDataAccess
 from database.mysql_access.implements.AddressDataAccess import AddressDataAccess
-from database.mysql_access.implements.AddressDataAccess import AddressDataAccess
-from database.mysql_access.models import db, City, District, Brand, Category, Address, Color, Store
+from database.mysql_access.models import db, City, District, Brand, Category, Address, Color, Store, Product
 
 class AccessFactory:
     def __init__(self):
@@ -19,6 +19,8 @@ class AccessFactory:
         self.address_access = AddressDataAccess(self.db, Address, District, City)
         self.color_access = ColorDataAccess(self.db, Color)
         self.store_access = StoreDataAccess(self.db, Store, Address)
+        self.product_access = ProductDataAccess(self.db, Product, Category)
+
 
     def get_access(self, access_type):
         if access_type == "city":
@@ -35,6 +37,9 @@ class AccessFactory:
             return self.color_access
         elif access_type == "store":
             return self.store_access
+        elif access_type == "product":
+            return self.product_access
+
 
     def init_app(self, app):
         self.db.init_app(app)
