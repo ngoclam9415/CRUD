04ff93f18a3cf4ea720d15b4a33953e7f2ca6513
Nguyen Ngoc Lam <lamnn@athena.studio>
2019-11-19 11:00:48 +0700

    Finish Refractor Category

--- app/CRUD/category/views.py
+++ app/CRUD/category/views.py
@@ -1,7 +1,8 @@
-from flask import Blueprint, render_template, request, make_response, jsonify, redirect
+from flask import Blueprint, render_template, request, make_response, jsonify, redirect, url_for
 
 from database.mysql_access.models import db
 from database.mysql_access.models import City, Category, Brand
+from database import access_factory
 
 category_blueprint = Blueprint('category', __name__, template_folder='templates')
 
@@ -9,30 +10,17 @@ category_blueprint = Blueprint('category', __name__, template_folder='templates'
 @category_blueprint.route('/api/list', methods=['GET'])
 def list_category_api():
     page = request.args.get('page', 1, type=int)
-    categories = Category.query.paginate(page, 10, error_out=False)
-    total_pages = categories.pages
-    arr_category = []
-    for category in categories.items:
-        tmp_category = {
-            'id': category.id,
-            'name': category.name,
-            'brand_name': Brand.query.get_or_404(int(category.brand_id)).name
-        }
-        arr_category.append(tmp_category)
-    res = {
-        "total_pages": total_pages,
-        "data": arr_category,
-    }
+    res = access_factory.get_access("category").list_item(page=page)
     return make_response(jsonify(res), 200)
 
 
 @category_blueprint.route('/', methods=['GET'])
 def list_category():
+    error = request.args.get("error", None)
+    print(error)
     page = request.args.get('page', 1, type=int)
-    categories = Category.query.paginate(page, 10, error_out=False)
-    total_pages = categories.pages
-    brands = Brand.query.all()
-    return render_template('CRUD/category/list.html', total_pages=total_pages, brands=brands, category_active="active")
+    res = access_factory.get_access("category").list_item(page=page)
+    return render_template('CRUD/category/list.html', total_pages=res["total_pages"], brands=res["brands"], category_active="active", error=error)
 
 
 @category_blueprint.route('/create', methods=['GET', 'POST'])
@@ -41,11 +29,8 @@ def create_category(error=None):
     if request.method == 'POST':
         category_name = request.form['category_name']
         brand_id = request.form['brand_id']
-        category_exist = Category.query.filter_by(name=category_name).first()
-        if category_exist is None and category_name != "":
-            new_category = Category(name=category_name, brand_id=brand_id)
-            db.session.add(new_category)
-            db.session.commit()
+        if category_name != "" and access_factory.get_access("category").verify_qualified_item(name=category_name, brand_id=brand_id) :
+            access_factory.get_access("category").create_item(name=category_name, brand_id=brand_id)
             return redirect('/category')
         else:
             error = "Your category is error"
@@ -54,9 +39,11 @@ def create_category(error=None):
 
 @category_blueprint.route('/edit', methods=['POST'])
 def edit_category():
-    category_id = request.form['category_id']
+    category_id = request.form.get('category_id', type=int)
     category_name = request.form['category_name']
-    brand_id = request.form['brand_id']
-    db.session.query(Category).filter(Category.id == category_id).update({"name": category_name, "brand_id": int(brand_id)})
-    db.session.commit()
+    brand_id = request.form.get('brand_id', type=int)
+    if category_name != "" and access_factory.get_access("category").verify_qualified_item(name=category_name, brand_id=brand_id):
+        access_factory.get_access("category").edit_item(category_id, name=category_name, brand_id=brand_id)
+    else:
+        return redirect(url_for("category.list_category", error="EDIT INPUT INVALID"))
     return redirect('/category')
